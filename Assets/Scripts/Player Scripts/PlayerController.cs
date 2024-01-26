545a42d5edf9d670790395a1f6d937a753c68399
Infinity <95947686+InfinitynBeyondd@users.noreply.github.com>
2024-01-26 15:55:06 -0500

    Added Assets inside project

---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
+    private bool isMoving = false;
+    private Vector2 input;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
