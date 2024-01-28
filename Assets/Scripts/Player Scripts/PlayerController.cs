5ee04609100dd0a247f4cbc6ac6a094135f16fc2
Infinity <95947686+InfinitynBeyondd@users.noreply.github.com>
2024-01-28 15:10:11 -0500

    Added Marios Level 1 and basic player character movement

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CharacterController2D : MonoBehaviour
 {
-    public float MoveSpeed;
-    private bool isMoving = false;
-    private Vector2 input;
+    public float moveSpeed = 5f; // Speed of the character movement
 
+    private Rigidbody2D rb2d; // Reference to the Rigidbody2D component
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>(); // Getting the Rigidbody2D component attached to the GameObject
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get horizontal and vertical input from the player
+        float moveInputX = Input.GetAxis("Horizontal");
+        float moveInputY = Input.GetAxis("Vertical");
+
+        // Calculate the velocity vector
+        Vector2 moveVelocity = new Vector2(moveInputX * moveSpeed, moveInputY * moveSpeed);
+
+        // Apply the velocity to the Rigidbody2D
+        rb2d.velocity = moveVelocity;
+
+        // Update the sprite's position based on Rigidbody2D's velocity
+        transform.position += (Vector3)moveVelocity * Time.deltaTime;
     }
 }
