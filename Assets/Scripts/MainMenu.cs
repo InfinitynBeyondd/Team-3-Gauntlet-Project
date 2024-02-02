64c91a77924f52849ac351e3eeed52e669c9b682
Kanewuu <wadehfla@gmail.com>
2024-02-02 14:28:10 -0500

    Updates to main menu and character select

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Update()
+    {
+        // Check for key press to load the scene
+        if (Input.GetKeyDown(KeyCode.Return)) // Use KeyCode.Alpha1 instead of KeyCode.1
+        {
+            LoadCharacterSelectScene();
+        }
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Character_Select");
+        LoadCharacterSelectScene();
+    }
+
+    private void LoadCharacterSelectScene()
+    {
+        // Load the scene asynchronously
+        SceneManager.LoadSceneAsync("Character_SelectKnight");
     }
 }
